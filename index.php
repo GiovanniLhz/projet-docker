0ae95f6abe8076423e258132833bd925c7b4885c
GiovanniLhz <giovannilihanza@gmail.com>
2026-09-24 23:48:01 +0200

    feat: conteneurisation de l'application LAMP avec Dockerfile

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Projet Docker - Sciensano</h1>";
+echo "<p>Application conteneurisée déployée  sur Apache et PHP . </p>";
+echo "<p><strong>Version de PHP :</strong> " . phpversion() . "</p>";
+?>
